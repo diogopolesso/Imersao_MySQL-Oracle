6444ebeaf6da856ea33ff08d7a3d7e8cc2c14ee7
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-15 19:27:39 -0300

    INSERT tabela Vendedores

---
+++ INSERT_Tabela-vendedores.sql
@@ -0,0 +1,34 @@
+INSERT INTO tabela_de_vendedores
+(
+MATRICULA,
+NOME,
+PERCENTUAL_COMISSÃO
+) VALUES (
+'00233',
+'João Geraldo da Fonseca',
+0.10
+);
+
+INSERT INTO tabela_de_vendedores
+(
+MATRICULA,
+NOME,
+PERCENTUAL_COMISSÃO
+) VALUES (
+'00235',
+'Márcio Almeida Silva',
+0.08
+);
+
+INSERT INTO tabela_de_vendedores
+(
+MATRICULA,
+NOME,
+PERCENTUAL_COMISSÃO
+) VALUES (
+'00236',
+'Claúdia Morais',
+0.08
+);
+
+SELECT * FROM tabela_de_vendedores
