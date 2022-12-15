7a776fa4b7908d115e4cab1dec11dbb2544cf6f3
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-15 19:31:16 -0300

    INSERT INTO Tabela Produtos

---
+++ INSERT_INTO_Tabela-Produtos.sql
@@ -0,0 +1,69 @@
+INSERT INTO tbproduto
+(
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PREÇO
+) VALUES (
+'1040107',
+'Light',
+'Lata',
+'350 ml',
+'Melância',
+4.56
+);
+
+INSERT INTO tbproduto
+(
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PREÇO
+) VALUES (
+'1037797',
+'PET',
+'Clean',
+'2 Litros',
+'Laranja',
+16.01
+);
+
+INSERT INTO tbproduto
+(
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PREÇO
+) VALUES (
+'1000889',
+'Sabor da Montanha',
+'Garrafa',
+'700 ml',
+'Uva',
+6.31
+);
+
+INSERT INTO tbproduto
+(
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PREÇO
+) VALUES (
+'1004327',
+'Videira do Campo',
+'PET',
+'1,5 Litros',
+'Melância',
+19.51
+);
+
+SELECT * FROM tbproduto;
