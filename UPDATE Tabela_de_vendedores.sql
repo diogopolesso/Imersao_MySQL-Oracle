02cbc16ea1052dbb382b7e0444e22191207d0506
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-16 14:33:09 -0300

    UPDATE Tabela de vendedores

---
+++ UPDATE Tabela_de_vendedores.sql
@@ -0,0 +1,5 @@
+UPDATE Tabela_de_vendedores SET PERCENTUAL_COMISSÃO = 0.11
+WHERE MATRICULA = '00236';
+
+UPDATE Tabela_de_vendedores SET NOME = 'José Geraldo da Fonseca Junior'
+WHERE MATRICULA = '00233';
