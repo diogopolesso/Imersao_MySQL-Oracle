cb77bdb3e68261637a57871d8ea570e7e0368d34
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-16 13:42:44 -0300

    UPDATE SET Produtos

---
+++ UPDATE_SET-Produtos.sql
@@ -0,0 +1,5 @@
+UPDATE tbproduto SET NOME = 'Frescor do Verão', EMBALAGEM = 'Lata', PREÇO = 2.46
+WHERE PRODUTO = '544931';
+
+UPDATE tbproduto SET NOME = 'Frescor do Verão', EMBALAGEM = 'Garrafa'
+WHERE PRODUTO = '1078680';
