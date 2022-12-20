577158a78b23f4982d02866dd3a5748f796da51e
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-20 14:58:24 -0300

    AND - OR - NOT

---
+++ AND-OR-NOT.sql
@@ -0,0 +1,13 @@
+A expressão abaixo é verdadeira ou falsa?
+
+(NOT ((3 > 2) OR (4 >= 5)) AND (5 > 4) ) OR (9 > 0)
+
+Resultado:
+
+(NOT ((3 > 2) OR (4 >= 5)) AND (5 > 4) ) OR (9 > 0)
+(NOT ((Verdadeiro) OR (Falso)) AND (Verdadeiro) ) OR (Verdadeiro)
+(NOT (Verdadeiro) AND (Verdadeiro) ) OR (Verdadeiro)
+(Falso AND Verdadeiro) OR (Verdadeiro)
+(Falso) OR (Verdadeiro)
+
+Verdadeira
