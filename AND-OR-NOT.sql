b88ad6034fc32929d2bdcb0427171b37adce54d6
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-20 14:59:07 -0300

    AND - OR - NOT

--- AND-OR-NOT.sql
+++ AND-OR-NOT.sql
@@ -11,3 +11,15 @@ Resultado:
 (Falso) OR (Verdadeiro)
 
 Verdadeira
+
+
+AND 'E' 'mostra um registro se ambas as condições forem verdadeiras'
+'EX.: V COM V = V, V COM F = F, F COM V = F, F COM F = F'
+
+OR 'OU' 'mostra um registro se pelo menos uma das condições for verdadeira'
+'EX.: V COM V = V, V COM F = V, F COM V = V, F COM F = F'
+
+NOT 'é a negação de uma expressão (inverte seu estado lógico)'.
+'EX.: V COM V = F, V COM F = V, F COM V = V, F COM F = V'
+
+'AND, OR e NOT são usados para filtrar registros baseados em mais de uma condição'.
