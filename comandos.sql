a35e6d6c7e4ba6bb73207da8313557b0d10b916e
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-22 18:31:55 -0300

    Comandos e Operadores SQL

--- comandos.sql
+++ comandos.sql
@@ -65,25 +65,45 @@ JOIN 'permitem unir duas ou mais tabelas dentro de uma única consulta SQL. Bast
 
 TIPOS DE JOIN:
 
-INNER JOIN 'buscar a intersecção entre as tabelas, Ou seja, resulta em uma tabela que traz os valores que estão em ambas as tabelas iniciais.'
+=> INNER JOIN 'buscar a intersecção entre as tabelas, Ou seja, resulta em uma tabela que traz os valores que estão em ambas as tabelas iniciais.'
 
 SELECT * FROM tabela_de_vendedores A INNER JOIN notas_fiscais B ON A.MATRICULA = B.MATRICULA;
 
-LEFT JOIN 'Resulta em uma tabela que exibe somente os itens da tabela da esquerda, incluindo os itens em interseção com a tabela direita. Contudo,
+SELECT A.MATRICULA, A.NOME, COUNT(*) FROM tabela_de_vendedores A INNER JOIN notas_fiscais B
+ON A.MATRICULA = B.MATRICULA GROUP BY A.MATRICULA, A.NOME;
+
+SELECT YEAR(DATA_VENDA), SUM(QUANTIDADE * PRECO) AS FATURAMENTO
+FROM notas_fiscais NF INNER JOIN itens_notas_fiscais INF 
+ON NF.NUMERO = INF.NUMERO
+GROUP BY YEAR(DATA_VENDA)
+
+=> LEFT JOIN 'Resulta em uma tabela que exibe somente os itens da tabela da esquerda, incluindo os itens em interseção com a tabela direita. Contudo,
 exclui da relação o resto da tabela da direita.'
 
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A LEFT JOIN notas_fiscais B ON A.CPF = B.CPF WHERE B.CPF IS NULL;
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A LEFT JOIN notas_fiscais B ON A.CPF = B.CPF 
+WHERE B.CPF IS NULL AND YEAR (B.DATA_VENDA) = 2015;
+
+=> RIGHT JOIN 'mesma lógica do LEFT, só que para a outra tabela. Pega os dados completos da tabela da direita, incluindo os que também estão na tabela da direita.'
+
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A RIGHT JOIN notas_fiscais B ON A.CPF = B.CPF 
+WHERE B.CPF IS NULL AND YEAR (B.DATA_VENDA) = 2015;
+
+SELECT tabela_de_vendedores.BAIRRO, tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO, tabela_de_clientes.NOME FROM tabela_de_vendedores right JOIN tabela_de_clientes
+ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
+
+=> FULL JOIN 'comando para pegar todos os valores das duas tabelas e unir em uma única resultante.
+   É como pegar todos os produtos vendidos nas duas lojas da empresa e juntar todos esses registros para facilitar o controle ou a manipulação geral, por exemplo.'
 
 
-RIGHT JOIN 'mesma lógica do LEFT, só que para a outra tabela. Pega os dados completos da tabela da direita, incluindo os que também estão na tabela da direita.'
 
+=> CROSS JOIN 'executa uma operação mais complexa, em que todos os itens de uma tabela estarão associados a cada item da outra. '
 
-
-FULL JOIN 'comando para pegar todos os valores das duas tabelas e unir em uma única resultante.
-
-
-
-É como pegar todos os produtos vendidos nas duas lojas da empresa e juntar todos esses registros para facilitar o controle ou a manipulação geral, por exemplo.'
-CROSS JOIN 'executa uma operação mais complexa, em que todos os itens de uma tabela estarão associados a cada item da outra. '
-
+SELECT tabela_de_vendedores.BAIRRO,
+tabela_de_vendedores.NOME, DE_FERIAS,
+tabela_de_clientes.BAIRRO,
+tabela_de_clientes.NOME FROM tabela_de_vendedores, tabela_de_clientes;
 
 
