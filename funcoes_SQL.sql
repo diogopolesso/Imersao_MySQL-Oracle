8c77adb2da04969855c5f284a126556f61fc6b22
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-27 15:13:47 -0300

    Funções SQL

--- funcoes_SQL.sql
+++ funcoes_SQL.sql
@@ -4,3 +4,16 @@ SELECT LTRIM('    OLÁ') AS RESULTADO;
 SELECT LTRIM('OLÁ     ') AS RESULTADO;
 SELECT LTRIM('   OLÁ   ') AS RESULTADO;
 
+=> CONCAT 'adiciona duas ou mais expressões juntas.'
+
+SELECT CONCAT('OLÁ', ' ', 'TUDO BEM', '?') AS RESULTADO;
+
+=> UPPER 'converte uma string em letras maiúsculas.'
+
+SELECT UPPER('ola, tudo bem?') AS RESULTADO;
+
+=> SUBSTRING 'extrai uma substring de uma string (começando em qualquer posição).'
+
+SELECT SUBSTRING('OLÁ, TUDO BEM?', 5) AS RESULTADO;
+
+
