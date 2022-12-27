9188241f4e0d246d46916c6b6e60aa2506ff7b2a
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-27 17:11:48 -0300

    Relatório de Vendas Validas
    Relatório sobre vendas válidas e inválidas ao gerente de vendas para que possa ser analisado 
    se o parâmetro de volume de compras está sendo considerado durante as negociações com os clientes.

--- relatorio_de_vendas.sql
+++ relatorio_de_vendas.sql
@@ -1,10 +1,21 @@
-=>  Relatório sobre vendas válidas e inválidas ao gerente de vendas para que possa ser analisado 
-se o parâmetro de volume de compras está sendo considerado durante as negociações com os clientes. <=
+/* 'Relatório sobre vendas válidas e inválidas ao gerente de vendas para que possa ser analisado 
+se o parâmetro de volume de compras está sendo considerado durante as negociações com os clientes.' */
 
 SELECT * FROM ITENS_NOTAS_FISCAIS;
 SELECT * FROM NOTAS_FISCAIS;
 
-SELECT NF.CPF, DATE_FORMAT(NF.DATA_VENDA, '%Y-%m') AS MES_ANO, SUM(INF.QUANTIDADE) AS QUANTIDADE_VENDAS FROM NOTAS_FISCAIS NF
-INNER JOIN ITENS_NOTAS_FISCAIS INF
-ON NF.NUMERO = INF.NUMERO
-GROUP BY NF.CPF, DATE_FORMAT(NF.DATA_VENDA, '%Y-%m');
+select *from notas_fiscais NF
+inner join itens_notas_fiscais INF
+on NF.NUMERO = INF.NUMERO;
+
+/* Consulta com Vendas de Clientes por Mês */
+
+select NF.CPF, date_format(NF.DATA_VENDA, '%Y-%m') as MES_ANO,
+sum(INF.QUANTIDADE) as QUANTIDADE_VENDAS from notas_fiscais NF
+inner join itens_notas_fiscais INF on NF.NUMERO = INF.NUMERO
+GROUP BY NF.CPF, date_format(NF.DATA_VENDA, '%Y-%m');
+
+
+
+
+
