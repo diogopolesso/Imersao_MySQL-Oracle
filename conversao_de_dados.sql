da851f0d0e796f2d777943412b325c00da7aff7b
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2022-12-27 16:16:50 -0300

    Conversão de Dados SQL

--- conversao_de_dados.sql
+++ conversao_de_dados.sql
@@ -1,1 +1,7 @@
+REFERENCIAS COMPLETAS: https://dev.mysql.com/doc/refman/5.7/en/date-and-time-functions.html#function_date-format
+
 DATE_FORMAT(date,format) 'Formata o datevalor de acordo com a formatstring.'
+
+SELECT CONCAT('O Dia de Hoje é: ',  DATE_FORMAT(CURRENT_TIMESTAMP(),'%W, %d/%c/%Y') ) AS RESULTADO;
+
+SELECT SUBSTRING(CONVERT(23.3, CHAR), 1,1) AS RESULTADO;
