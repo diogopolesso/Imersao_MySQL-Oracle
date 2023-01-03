10ef90e57c61b4eb18f0043828fde19e0ce3329b
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-03 18:50:16 -0300

    Criando Chaves Estrangeiras

--- Chave_estrangeira.sql
+++ Chave_estrangeira.sql
@@ -5,3 +5,9 @@ foreign key (CPF) references cliente (CPF);
 
 alter table vendas add constraint fk_vendedores
 foreign key (MATRICULA) references vendedores (MATRICULA);
+
+alter table itens_notas add constraint fk_vendas
+foreign key (NUMERO) references vendas (NUMERO);
+
+alter table itens_notas add constraint fk_produtos
+foreign key (CODIGO) references produtos (CODIGO);
