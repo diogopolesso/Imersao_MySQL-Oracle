7851aa4e04fe29a0824ea3f2e22fcdbef8b1d525
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-04 17:56:28 -0300

    Inclusão de Registros na Tabela Cliente

---
+++ INSERT-INTO-CLIENTE.sql
@@ -0,0 +1,8 @@
+use vendas_sucos;
+
+insert into cliente
+values ('1471156710', 'Érica Carvalho', 'R. Iriquitia', 'Jardins', 'São Paulo', 'SP', '80012212', '1990-09-01', 27, 'F', 170000, 24500, 0),
+('19290992743', 'Fernando Cavalcante', 'R. Dois de Fevereiro', 'Água Santa', 'Rio de Janeiro', 'RJ', '22000000', '2000-02-12', 18, 'M', 100000, 20000, 1),
+('2600586709', 'César Teixeira', 'Rua Conde de Bonfim', 'Tijuca', 'Rio de Janeiro', 'RJ', '22020001', '2000-03-12', 18, 'M', 120000, 22000, 0);
+
+select * from cliente;
