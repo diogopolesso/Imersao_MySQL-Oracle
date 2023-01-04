9a602ee39d7c5f3fa6f704965908a4c8c58bb71e
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-04 18:07:53 -0300

    Incluindo Múltiplos Registros

---
+++ MULTIPLOS-REGISTROS.sql
@@ -0,0 +1,12 @@
+/* dentro de uma empresa é muito normal, 
+às vezes eu estou criando um banco de dados e eu quero importar estes dados de um outro lugar. 
+Aqui no caso eu vou importar de um outro banco mysql. 
+Então a gente vai ver como é que a gente faz isso: */
+
+use vendas_sucos;
+
+select * from sucos_vendas.tabela_de_produtos;
+
+select CODIGO_DO_PRODUTOS as CODIGO, NOME_DO_PRODUTO as DESCRITOR,
+EMBALAGEM, TAMANHO, SABOR, PRECO_DE_LISTA as PRECO_LISTA
+from sucos_vendas.tabela_de_produtos;
