b14b01bf75ae09587fd3dadb9ea17a64daf293d8
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 17:08:11 -0300

    CREATE STATEMENT

---
+++ CREATE-STATEMENT.sql
@@ -0,0 +1,16 @@
+/* Com o botão direito do mouse sobre a tabela de produtos.
+tenho uma série de opções que me ajudam a construir alguns comandos de SQL.
+por exemplo: eu escolho a opção: Send to SQL Editor.
+E eu vou escolher a opção Create Statement.
+Ao fazer isso, ele vai escrever o comando de SQL 
+para criar uma tabela igual aquela tabela que já existe: */
+
+CREATE TABLE `produtos` (
+  `CODIGO` varchar(10) NOT NULL,
+  `DESCRITOR` varchar(100) DEFAULT NULL,
+  `SABOR` varchar(50) DEFAULT NULL,
+  `TAMANHO` varchar(50) DEFAULT NULL,
+  `EMBALAGEM` varchar(50) DEFAULT NULL,
+  `PRECO_LISTA` float DEFAULT NULL,
+  PRIMARY KEY (`CODIGO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
