d72336733299a505a323f98c9265b3d18729f6d0
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 15:10:28 -0300

    Modificando Dados da Tabela com Update

---
+++ UPDATE-SET.sql
@@ -0,0 +1,6 @@
+/* Modificando Dados da Tabela com Update, 
+O comando UPDATE modifica um dado já existente, 
+e basicamente a gente dá update, o nome da tabela, 
+o conjunto de campos que serão modificados com os seus novos valores e uma condição de filtro. */
+
+update produtos set PRECO_LISTA = 5 where CODIGO = '1000889';
