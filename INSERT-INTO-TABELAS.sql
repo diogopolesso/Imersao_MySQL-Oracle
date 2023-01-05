19d028ce37b7afc5f7e66e40038ffa1d54253523
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 17:21:07 -0300

    Alterando e apagando toda a tabela
    - Transferir o conteúdo de uma tabela para outra.
    - Alterar tudo, todas as linhas num determinado campo.
    - Apagar Toda Tabela.

--- INSERT-INTO-TABELAS.sql
+++ INSERT-INTO-TABELAS.sql
@@ -3,3 +3,18 @@ que eu tenho na tabela de 'produto' para a tabela de 'produto2': */
 
 insert into produtos2
 select * from produtos;
+
+
+/* Alterar tudo, todas as linhas num determinado campo: */
+
+update produtos2 set PRECO_LISTA = 8; 
+
+/* Note que nesse comando aqui update, 
+eu não coloquei um WHERE. Isso significa que ele vai fazer essa alteração em toda a tabela. */
+
+/* Apagar a tabela, apagar o conteúdo dela. */
+
+delete from produtos2;
+
+/* tomar cuidado realmente com qualquer delete, mas o delete from sem o WHERE, 
+você vai perder tudo que você tem dentro da sua tabela e não tem como recuperar. */
