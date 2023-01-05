06f923e1751cde7275e8009186e451a2113aa948
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 15:24:15 -0300

    Modificando Dados da Tabela com Update

--- UPDATE-SET.sql
+++ UPDATE-SET.sql
@@ -4,3 +4,6 @@ e basicamente a gente dá update, o nome da tabela,
 o conjunto de campos que serão modificados com os seus novos valores e uma condição de filtro. */
 
 update produtos set PRECO_LISTA = 5 where CODIGO = '1000889';
+
+update produtos set DESCRITOR = 'Sabor da Montanha - 1 Litro - Uva', 
+TAMANHO = '1 Litro', EMBALAGEM = 'PET' where CODIGO = '1000889';
