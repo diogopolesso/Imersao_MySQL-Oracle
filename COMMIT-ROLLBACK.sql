8b49c29511cc6eeb45d1e52a77d629e2690f621c
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 17:49:44 -0300

    Transação no MySQL

--- COMMIT-ROLLBACK.sql
+++ COMMIT-ROLLBACK.sql
@@ -8,3 +8,20 @@ start transaction /* Cria um Ponto de estado do Banco de Dados */
 commit  /* confirmo no banco de dados tudo aquilo que eu modifiquei depois do START TRANSACTION. */
 rollback /*  volto atrás e não consigo ver as modificações, as modificações não são confirmadas no banco. */
 
+/* START TRANSACTION E ROLLBACK: VOU INICIAR UMA TRANSAÇÃO E DEPOIS VOU VOLTAR A TRÁS NAS MINHA ALTERAÇÃO E NÃO VOU CONFIRMAR AS ALTERAÇÕES */
+
+start transaction;
+
+select * from vendedores;
+
+update vendedores set COMISSAO = COMISSAO * 1.15;
+
+rollback;
+
+/* START TRANSATION E COMMIT: VOU INICIAR UMA TRANSAÇÃO E DEPOIS VOU CONFIRMAR AS ALTERAÇÕES */
+
+start transaction;
+
+update vendedores set COMISSAO = COMISSAO * 1.15;
+
+commit;
