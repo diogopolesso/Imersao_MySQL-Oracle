81c7ced25e7a186c0a99781f9cc146b1a6441b19
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-05 16:28:29 -0300

    UPDATE com FROM

--- UPDATE-FROM.sql
+++ UPDATE-FROM.sql
@@ -5,9 +5,17 @@ select * from vendedores;
 
 select * from sucos_vendas.tabela_de_vendedores;
 
+/* Na tabela VENDEDORES eu tenho o campo matrícula com 3 digitos 
+e na TABELA_DE_VENDEDORES do outro banco eu tenho o campo matrícula com dois zeros + 3 digitos. 
+Esses dois zeros na TABELA_DE_VENDEDORES tem que ser tirado na hora que eu for fazer o join, 
+e aí a função que eu uso é a função substring. */
+
 select * from vendedores A 
 inner join sucos_vendas.tabela_de_vendedores B 
 on A.MATRICULA = substring(B.MATRICULA,3,3);
+
+/* Eu pego update, o nome da tabela que eu quero modificar, VENDEDORES, que é a tabela A, aplico o inner join,
+inner join com outra a tabela ou campo e aí eu coloco meu set, e depois o campo que vai ser modificados, no caso B.DE_FERIAS. */
 
 update vendedores A inner join sucos_vendas.tabela_de_vendedores B 
 on A.MATRICULA = substring(B.MATRICULA,3,3)
