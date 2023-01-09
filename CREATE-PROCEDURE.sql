ac0bcead7e8efeb69a5c30845591a40595f5ee8f
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-09 17:13:08 -0300

    create procedures

--- CREATE-PROCEDURE.sql
+++ CREATE-PROCEDURE.sql
@@ -10,3 +10,6 @@ END$$
 
 DELIMITER ;
 
+
+/* OBS: Temos que mudar o delimitador para que não haja uma confusão entre os comandos da SP (Stored Procedure).
+   Porque se continuarmos a usar o ponto e vírgula haverá uma confusão entre o ponto e vírgula dos comandos da Stored Procedure e dos comandos para criá-la */
