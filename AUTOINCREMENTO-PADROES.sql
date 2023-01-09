2c52bde78bdbb2df6038e5b27180494bf6c45528
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-09 15:13:15 -0300

    Definindo padrões para os campos

--- AUTOINCREMENTO-PADROES.sql
+++ AUTOINCREMENTO-PADROES.sql
@@ -3,7 +3,7 @@
 caso ele não seja mencionado no Insert.
 Podemos definir outros padrões para campos:*/
 
-create table tab_padrão
+create table tab_padrao
 (ID int auto_increment, 
 DESCRITOR varchar(20),
 ENDERECO varchar(100) null,
@@ -11,6 +11,11 @@ CIDADE varchar(50) default 'Rio de Janeiro',
 DATA_CRIACAO timestamp default current_timestamp(),
 primary key(ID));
 
+/* OBS: Lembrando que timestamp = DATA/HORA/MIN/SEG e Função current_timestamp()= DATA/HORA DO COMPUTADOR 
+O campo CIDADE, ele tem como Default 'Rio de Janeiro', se eu não falar nada durante o Insert, o valor a ser colocado será 'Rio de Janeiro' */
 
-/* Lembrando que timestamp = DATA/HORA/MIN/SEG e Função current_timestamp()= DATA/HORA DO COMPUTADOR*/
+insert into tab_padrao (DESCRITOR, ENDERECO, CIDADE, DATA_CRIACAO)
+values ('Cliente X', 'Rua Projetadas A', 'São Paulo', '2022-01-09');
+
+select * from tab_padrao;
 
