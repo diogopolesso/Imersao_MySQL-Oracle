b0487504f157296dc4edd0750595de68b2048542
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-10 14:13:33 -0300

    Declarando variáveis

--- STORED-PROCEDURES-VARIAVEL.sql
+++ STORED-PROCEDURES-VARIAVEL.sql
@@ -1,4 +1,15 @@
 /* As variáveis, elas são declaradas através do comando "DECLARE", o nome da variável e um "DATATYPE", que é um tipo.
 O "DATATYPE", ele é obrigatório ter. Toda variável tem que ter um tipo, mas ela, não necessariamente, precisa ter um valor "DEFAULT".
-O nome da variável deve ser apenas letras, números, $ e o underscore. */
+Se eu não tiver um valor "DEFAULT" para a variável, o valor dela será NULL.
+O nome da variável deve ser apenas letras, números, $ e o underscore. E o tamanho máximo do nome da variável é 255 caracteres. */
+
+-- Tipos de Variaveis:
+
+VARCHAR()
+INTEGER
+DECIMAL(p,s)
+DATE
+TIMESTAMP
+
+
 
