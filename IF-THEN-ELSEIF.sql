ba319a588a27df46f6c5f6daed9174bcf8a7b93d
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-11 16:29:21 -0300

    Controle de Fluxo  "IF/THEN/ ELSEIF"

--- IF-THEN-ELSEIF.sql
+++ IF-THEN-ELSEIF.sql
@@ -10,3 +10,33 @@ ELSEIF
 ELSE
   else_stantements;
 END IF
+
+
+-- EXEMPLO:
+
+DROP procedure IF EXISTS `achar_status_preco`;
+
+DELIMITER $$
+USE `sucos_vendas`$$
+CREATE PROCEDURE `achar_status_preco` (vProduto varchar(50))
+BEGIN
+declare vPreco float;
+declare vMensagem varchar(30);
+select PRECO_DE_LISTA into vPreco from tabela_de_produtos where CODIGO_DO_PRODUTO = vProduto;
+IF vPreco >= 12 THEN
+	set vMensagem = 'Produto Caro!';
+ELSEIF vPreco >= 7 and vPreco < 12 THEN
+	set vMensagem = 'Produto Em Conta!';
+ELSE 
+	set vMensagem = 'Produto Barato!';
+END IF;
+	select vMensagem;
+END$$
+
+DELIMITER ;
+
+call call achar_status_preco('1000889'); --RESULTADO = Produto Barato!
+
+call call achar_status_preco('1086543'); --RESULTADO = Produto Em Conta!
+
+call call achar_status_preco('326779'); --RESULTADO = Produto Caro!
