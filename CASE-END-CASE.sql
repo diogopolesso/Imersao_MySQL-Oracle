296ba3f95009a3f185b0b49c14343c1830d25423
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-11 16:32:39 -0300

    Controle de Fluxo "CASE"

---
+++ CASE-END-CASE.sql
@@ -0,0 +1,11 @@
+-- Controle de Fluxo "CASE".
+
+CASE selector
+WHEN selector_value_1 THEN then_stantement_1;
+WHENselector_value_1 THEN then_stantement_2;
+WHEN ....
+[ELSE else_stantements]
+END CASE;
+
+-- EXEMPLO:
+
