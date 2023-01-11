5a81cabd7b78f4bf0d30eafe1c75e6870f25c854
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-11 15:02:47 -0300

    Controle de Fluxo  "IF/THEN/ ELSEIF"

---
+++ IF-THEN-ELSEIF.sql
@@ -0,0 +1,12 @@
+-- Controle de Fluxo  "IF/THEN/ ELSEIF".
+
+IF condition THEN
+  if_stantements;
+ELSEIF
+  elseif_stantements;
+ELSEIF
+  elseif_stantements;
+(....)
+ELSE
+  else_stantements;
+END IF
