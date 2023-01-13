63cefb516e0d3de5ce1d304ada4a0c64cb99cbf2
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-13 12:50:34 -0300

    Controle de Fluxo "CASE"

--- CASE-END-CASE.sql
+++ CASE-END-CASE.sql
@@ -9,3 +9,27 @@ END CASE;
 
 -- EXEMPLO:
 
+DROP procedure IF EXISTS `acha_tipo_sabor`;
+
+DELIMITER $$
+USE `sucos_vendas`$$
+CREATE PROCEDURE `acha_tipo_sabor` (vProduto varchar(50))
+BEGIN
+	declare vSabor varchar(50);
+	select SABOR into vSabor from tabela_de_produtos 
+	where CODIGO_DO_PRODUTO = vProduto;
+	CASE vSabor
+	WHEN 'Lima/Limão' THEN select 'Cítrico';
+	WHEN 'Laranja' THEN select 'Cítrico';
+	WHEN 'Morango/Limão' THEN select 'Cítrico';
+	WHEN 'Uva' THEN select 'Neltro';
+	WHEN 'Morango' THEN select 'Neltro';
+	ELSE select 'Acidos';
+	END CASE;
+END$$
+
+DELIMITER ;
+
+call acha_tipo_sabor('1000889'); -- (Uva) RESULTADO = Neltro 
+
+call acha_tipo_sabor('1002334'); -- (Lima/Limão) RESULTADO = Cítrico
