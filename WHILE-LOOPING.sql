ce8b443986e2a92665af93169f8c2b59352e9670
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-13 13:06:59 -0300

    Controle de Fluxo "WHILE-DO-END"

---
+++ WHILE-LOOPING.sql
@@ -0,0 +1,22 @@
+-- Controle de Fluxo "WHILE-DO-END":
+
+WHILE condition
+DO stantements;
+END WHILE;
+
+-- EXEMPLO;
+
+-- vou criar aqui uma tabela chamada "TAB_LOOPING", que vai ter só um campo "ID" que vai ser do tipo inteiro:
+
+create table tab_looping (id int);
+
+/*
+minha rotina com 2 Parâmetros:
+um número inicial e um número final. 
+Vou fazer um contador onde vou contar a partir do número inicial, 
+com intervalos de 1 a 1, até o número final.
+Cada interação eu incluo o valor da sequência dentro dessa tabela.
+*/
+
+
+
