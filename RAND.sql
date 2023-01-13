b555eabfd76eca41a607a83d59ee075dd0bf982f
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-13 18:53:52 -0300

    Criando números aleatórios

---
+++ RAND.sql
@@ -0,0 +1,24 @@
+/* Função Interna "RAND":
+quando executamos a função "RAND", ela te mostra um número aleatório entre 0 e 1. */
+
+select rand();
+
+select floor(rand() * (300-15+1) + 15);
+
+ -- EXEMPLO PRATICO:
+ 
+ DROP function IF EXISTS `f_numero_aleatorio`;
+
+DELIMITER $$
+USE `sucos_vendas`$$
+CREATE FUNCTION `f_numero_aleatorio` (min int, max int)
+RETURNS INTEGER
+BEGIN
+declare vRetorno int;
+select floor(rand() * (max-min+1) + min) into vRetorno;
+RETURN vRetorno;
+END$$
+
+select f_numero_aleatorio (1,10); -- RESULTADO = busca um número aleatório entre 1 e 10
+select f_numero_aleatorio (1,15); -- RESULTADO = busca um número aleatório entre 1 e 15
+select f_numero_aleatorio (15 ,300); -- RESULTADO = busca um número aleatório entre 15 e 300
