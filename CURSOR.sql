52dc06cbbb95eacbfe923090a29c5bfa17da3aff
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-13 14:14:37 -0300

    Definição de Cursor

---
+++ CURSOR.sql
@@ -0,0 +1,21 @@
+-- Definição de Cursor:
+
+/*
+Como já sabemos não consigo associar uma variável quando o resultado do meu select for um vetor, o "SELECT INTO" não funciona nesses casos.
+O SELECT INTO somente pode ser aplicado quando a consulta retorna uma linha apenas.
+"Existe uma estrutura chamada "CURSOR" que faz isso".
+
+o "CURSOR" é uma estrutura que é implementada no MySQL que permite uma interatividade linha a linha através de uma determinada ordem
+pelo comando select que define o "CURSOR".
+
+Fases:
+
+Primeiro declaro o "CURSOR".
+Depois, abrimos o "CURSOR".
+Percorrer linha a linha na ordem com que o cursor é organizado.
+E chego no final do "CURSOR".
+*/
+
+-- EXEMPLO PRATICO:
+
+
