999bd8684fd219e547d093a39c4068ad0f492404
diogopolesso <111795220+diogopolesso@users.noreply.github.com>
2023-01-16 14:31:03 -0300

    Função Interna "RAND" Obtendo CLIENTES Aleatórios

--- RAND-CLIENTES.sql
+++ RAND-CLIENTES.sql
@@ -11,17 +11,15 @@ DROP function IF EXISTS `f_cliente_aleatorio`;
 
 DELIMITER $$
 USE `sucos_vendas`$$
-CREATE FUNCTION `f_cliente_aleatorio` () returns int (11)
-RETURNS INTEGER
+CREATE DEFINER=`root`@`localhost` FUNCTION `f_cliente_aleatorio`() RETURNS varchar(11)
 BEGIN
-	declare vRetorno varchar(11);
-	declare num_max_tabela int;
-	declare num_aleatorio int;
-	select count(*) into num_max_tabela from tabela_de_clientes;
-	set num_aleatorio = f_numero_aleatorio(1, num_max_tabela);
-	set num_aleatorio = num_aleatorio - 1;
-	select CPF into vRetorno from tabela_de_clientes
-	limit num_aleatorio, 1;
+    declare vRetorno varchar(11);
+    declare num_max_tabela int;
+    declare num_aleatorio int;
+    select count(*) into num_max_tabela from tabela_de_clientes;
+    set num_aleatorio = f_numero_aleatorio(1, num_max_tabela);
+    set num_aleatorio = num_aleatorio - 1;
+    select CPF into vRetorno from tabela_de_clientes limit num_aleatorio, 1;
 RETURN vRetorno;
 END$$
 
